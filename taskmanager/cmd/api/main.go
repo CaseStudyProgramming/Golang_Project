96e385070d6a821731214c827eba39800543dbef
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-03 22:14:17 +0700

    added main.go

--- taskmanager/cmd/api/main.go
+++ taskmanager/cmd/api/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"taskmanager/pkg/database"
+)
 
 func main() {
-	fmt.Println("halo")
+	// sementara hardcode dulu (nanti kita load dari config.yaml)
+	db := database.NewPostgresDB("localhost", 5432, "postgres", "berjuang02", "taskmanager", "disable")
+	defer db.Close()
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "API is running 🚀")
+	})
+
+	log.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
