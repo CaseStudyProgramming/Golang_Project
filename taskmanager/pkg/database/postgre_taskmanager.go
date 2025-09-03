7f104b6822c7b2895b638f6e34c587a6e7fa644f
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-03 21:34:30 +0700

    add db

--- taskmanager/pkg/database/postgre_taskmanager.go
+++ taskmanager/pkg/database/postgre_taskmanager.go
@@ -1,7 +1,29 @@
-package main
+package database
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-func main() {
-	fmt.Println("halo")
+	_ "github.com/lib/pq" // driver postgres
+)
+
+func NewPostgresDB(host string, port int, user, password, dbname, sslmode string) *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("failed to open db: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("failed to ping db: %v", err)
+	}
+
+	fmt.Println("✅ Connected to PostgreSQL")
+	return db
 }
