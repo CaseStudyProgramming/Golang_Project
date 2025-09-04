e8223c8e6090adc14f6f871cdaca976ae159ff2d
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-04 22:08:56 +0700

    add get data

--- taskmanager/internal/repository/task_repo.go
+++ taskmanager/internal/repository/task_repo.go
@@ -17,3 +17,22 @@ func (r *TaskRepository) Create(task *entity.Task) error {
 	query := `INSERT INTO tasks (title, completed) VALUES ($1, $2) RETURNING id, created_at`
 	return r.DB.QueryRow(query, task.Title, task.Completed).Scan(&task.ID, &task.CreatedAt)
 }
+
+func (r *TaskRepository) GetAll() ([]entity.Task, error) {
+	query := `SELECT id, title, completed, created_at FROM tasks ORDER BY id ASC`
+	rows, err := r.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []entity.Task
+	for rows.Next() {
+		var task entity.Task
+		if err := rows.Scan(&task.ID, &task.Title, &task.Completed, &task.CreatedAt); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+	return tasks, nil
+}
