cda5aa6b1c73f491055736f63f86eba206c0fc75
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-09 23:50:37 +0700

    add comment handler

--- taskmanager/internal/handler/task_handler.go
+++ taskmanager/internal/handler/task_handler.go
@@ -17,6 +17,7 @@ func NewTaskHandler(repo *repository.TaskRepository) *TaskHandler {
 	return &TaskHandler{Repo: repo}
 }
 
+// POST
 func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
 	var task entity.Task
 	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
