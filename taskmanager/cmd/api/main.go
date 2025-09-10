20020025e73e1ea041daed244ba70685b243f6c5
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-10 23:41:51 +0700

    add comment

--- taskmanager/cmd/api/main.go
+++ taskmanager/cmd/api/main.go
@@ -18,6 +18,7 @@ func main() {
 	TaskRepository := repository.NewTaskRepository(db)
 	TaskHandler := handler.NewTaskHandler(TaskRepository)
 
+	// e
 	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			TaskHandler.CreateTask(w, r)
