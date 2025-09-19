a2fdbfec440ef72e5a6c5bd15bd36aea7c73b36a
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-19 23:30:04 +0700

    add field db

--- taskmanager/internal/entity/task.go
+++ taskmanager/internal/entity/task.go
@@ -4,8 +4,12 @@ import "time"
 
 // Task entity
 type Task struct {
-	ID        int64     `json:"id"`
-	Title     string    `json:"title"`
-	Completed bool      `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
+	ID          int64      `json:"id"`
+	Title       string     `json:"title"`
+	SubTitle    string     `json:"sub_title,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Completed   bool       `json:"completed"`
+	DueDate     *time.Time `json:"due_date,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
 }
