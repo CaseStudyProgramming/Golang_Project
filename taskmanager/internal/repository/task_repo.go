65a7b0d261dd776cdadc07c71591aed9d07feba4
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-09-22 23:54:09 +0700

    add

--- taskmanager/internal/repository/task_repo.go
+++ taskmanager/internal/repository/task_repo.go
@@ -9,7 +9,7 @@ type TaskRepository struct {
 	DB *sql.DB
 }
 
-// CREATE REPOSITORY INSTA
+// CREATE REPOSITORY INSTANCE
 func NewTaskRepository(db *sql.DB) *TaskRepository {
 	return &TaskRepository{DB: db}
 }
