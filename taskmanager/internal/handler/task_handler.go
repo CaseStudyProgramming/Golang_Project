b9cb45deadb85b1ff84123793641531578893dcb
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-12-03 23:46:04 +0700

    /

--- taskmanager/internal/handler/task_handler.go
+++ taskmanager/internal/handler/task_handler.go
@@ -41,7 +41,7 @@ func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
 	response_test.SuccessResponse(w, http.StatusCreated, "Task created successfully", createdTask)
 }
 
-// GET ALL DATA123
+// GET ALL DATA12
 func (h *TaskHandler) GetAllTasks(w http.ResponseWriter, r *http.Request) {
 	tasks, err := h.Repo.GetAll()
 	if err != nil {
