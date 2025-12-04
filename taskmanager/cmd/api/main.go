a04fdc7fcae2da184a0714ff4cff94ec38bc5d90
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-12-04 23:02:19 +0700

    /

--- taskmanager/cmd/api/main.go
+++ taskmanager/cmd/api/main.go
@@ -51,7 +51,7 @@ func main() {
 
 	// health check endpoint
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		_ = r
+		_ = r //u
 		fmt.Fprintln(w, "API is runningggg 🚀")
 	})
 
