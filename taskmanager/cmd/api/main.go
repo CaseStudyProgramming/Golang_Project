0e3f32f267a24970b50a75696ef63639c0f5529e
CaseStudyProgramming <casestudyprogramming@gmail.com>
2025-12-06 23:37:35 +0700

    /

--- taskmanager/cmd/api/main.go
+++ taskmanager/cmd/api/main.go
@@ -55,6 +55,6 @@ func main() {
 		fmt.Fprintln(w, "API is runningggg 🚀")
 	})
 
-	log.Println("Server running at :8080") // s
+	log.Println("Server running at :8080") // st
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
