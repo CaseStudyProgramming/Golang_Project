35036bd4c8f0772d755e9200d3d47e5cb8492c8f
CaseStudyProgramming <casestudyprogramming@gmail.com>
2026-03-02 23:19:53 +0700

    data

--- backendGoVanilaTaskmanager/cmd/api/main.go
+++ backendGoVanilaTaskmanager/cmd/api/main.go
@@ -72,6 +72,6 @@ func main() {
 		fmt.Fprintln(w, "API is runningggg 🚀")
 	})
 
-	log.Println("Server running at :8080") // start server2
+	log.Println("Server running at :8080") // start server22
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
